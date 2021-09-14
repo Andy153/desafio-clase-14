f2d882266376fde373370128f64fd70baf9a308e
Andrés García Skinner <andresgarciaskinner03@gmail.com>
2021-09-14 20:07:58 -0300

    nuevos cambios

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function GET() {
+    var apiurl = "https://dog.ceo/api/breeds/image/random";
+    var img = document.getElementById("perro-img");
+    fetch(apiurl)
+      .then((res) => res.json())
+      .then((data) => {
+        img.src = data.message;
+      });
+  }
+
